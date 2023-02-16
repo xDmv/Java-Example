53cba768c4cea14a3935826d5d363ea5bd9ac840
xDmv <makushin.dima@gmail.com>
2023-02-16 21:11:09 +0200

    update class MainHibernate

--- src/main/java/ua/edu/znu/hibernatestudy/entities/MainHibernate.java
+++ src/main/java/ua/edu/znu/hibernatestudy/entities/MainHibernate.java
@@ -6,24 +6,64 @@ import org.hibernate.SessionFactory;
 import org.hibernate.boot.MetadataSources;
 import org.hibernate.boot.registry.StandardServiceRegistry;
 import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import ua.edu.znu.hibernatestudy.entities.Roles;
+
+import java.time.Instant;
+import java.time.LocalDate;
+import java.util.LinkedHashSet;
+import java.util.List;
+import java.util.Set;
+
+import java.sql.Date;
+
 
 public class MainHibernate {
     public static void main(String[] args) {
-        Session session = null;
-        try {
-            StandardServiceRegistry registry =
-                    new StandardServiceRegistryBuilder()
-                            .configure()
-                            .build();
-            SessionFactory sessionFactory = new MetadataSources(registry)
-                    .buildMetadata()
-                    .buildSessionFactory();
-            session = sessionFactory.openSession();
+
+        try (Session session = getSessionFactory().openSession())  {
+            // add new item for Role
+            Roles role = new Roles();
+            role.setType("specialist");
+            role.setDescription("specialist in salon");
+            session.beginTransaction();
+            Long roleId = (Long) session.save(role);
+            System.out.println("add Role id: " + roleId);
+            session.getTransaction().commit();
+
+            // remove new item for Role
+            Roles roleDelete = session.get(Roles.class, roleId);
+            session.beginTransaction();
+            session.delete(roleDelete);
+            session.getTransaction().commit();
+
+            // update item for Role
+            Roles roleToUpdate = session.get(Roles.class, 6L);
+            session.evict(roleToUpdate);
+            roleToUpdate.setEdited_ed(Instant.now());
+            roleToUpdate.setAuthor_id(2L);
+            roleToUpdate.setType("specialist");
+            roleToUpdate.setDescription("type specialist");
+            roleToUpdate.setCreated_at(Instant.now());
+            session.beginTransaction();
+            session.update(roleToUpdate);
+//            session.saveOrUpdate(roleToUpdate);
+            session.getTransaction().commit();
+
         } catch (HibernateException e) {
             e.printStackTrace();
-        } finally {
-            if (session != null && session.isOpen()) {
-                session.close();
-            }
-        } }
+        }
+    }
+
+    private static SessionFactory getSessionFactory() {
+/*Load configuration data from src/main/resources/hibernate.cfg.xml
+          and build services registry use it*/
+        StandardServiceRegistry registry = new StandardServiceRegistryBuilder()
+                .configure()
+                .build();
+        /*Build session factory using services registry*/
+        SessionFactory sessionFactory = new MetadataSources(registry)
+                .buildMetadata()
+                .buildSessionFactory();
+        return sessionFactory;
+    }
 }
